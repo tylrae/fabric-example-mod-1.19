2ef57d9c7a70ece8bdc0e87fd2c0d7c72dbd4b04
tylrae <tylrae@outlook.com>
2022-09-03 17:47:46 +1000

    added important comment

--- src/main/java/net/tylrae/smokeybux/SmokeyBux.java
+++ src/main/java/net/tylrae/smokeybux/SmokeyBux.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Very important comment
 public class SmokeyBux implements ModInitializer {
 	public static final String MOD_ID = "smokeybux";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
